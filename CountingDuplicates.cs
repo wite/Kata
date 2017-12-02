869abe4fa7a18cc40d6d0f67ef4c434aae8de1c5
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 18:44:00 +0100

    6 kyu https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1

---
+++ CountingDuplicates.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int DuplicateCount(string str)
+    {
+        string inputString = str.ToLower();
+        return inputString.GroupBy(x => x).Where(g => g.Count() > 1).Select(g => g.Key).Count();
+    }
+}
