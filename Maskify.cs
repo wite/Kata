4454b3cc25b14035f3d33e164d48a3db23236ec7
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 11:41:55 +0100

    https://www.codewars.com/kata/5412509bd436bd33920011bc

---
+++ Maskify.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public static class Kata
+{
+    // return masked string
+    public static string Maskify(string cc)
+    {
+        char[] chars = cc.ToCharArray();
+        for (int i = 0; i < cc.Length - 4; i++)
+        {
+            chars[i] = '#';
+        }
+        return new string(chars);
+    }
+}
