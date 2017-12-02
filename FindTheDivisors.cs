5c4b0dbcbeec8de5db22e559a8c11ef69382d4e9
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 11:51:12 +0100

    https://www.codewars.com/kata/544aed4c4a30184e960010f4

---
+++ FindTheDivisors.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System;
+public class Kata
+{
+    public static int[] Divisors(int n)
+    {
+        List<int> divisorsList = new List<int>();
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+            {
+                if (!divisorsList.Contains(i))
+                {
+                    divisorsList.Add(i);
+                }
+                if (!divisorsList.Contains(n / i))
+                {
+                    divisorsList.Add(n / i);
+                }
+            }
+        }
+        divisorsList.Sort();
+        var divisorsArray = divisorsList.ToArray();
+        return divisorsArray.Length > 0 ? divisorsArray : null;
+    }
+}
