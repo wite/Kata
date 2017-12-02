07b1a754afe305b90ad2688609ed80b3da9f6e36
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 12:56:39 +0100

    6 kyu https://www.codewars.com/kata/5526fc09a1bbd946250002dc

---
+++ FindTheParityOutlier.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        List<int> evens = new List<int>();
+        List<int> odds = new List<int>();
+        foreach (var integer in integers)
+        {
+            if (integer % 2 == 0)
+            {
+                evens.Add(integer);
+            }
+            else
+            {
+                odds.Add(integer);
+            }
+
+            if (evens.Count > odds.Count && odds.Count == 1) return odds.First();
+            if (odds.Count > evens.Count && evens.Count == 1) return evens.First();
+        }
+        return 0;
+    }
+}
--- TakeATenMinuteWalk.cs
+++ TakeATenMinuteWalk.cs
@@ -1,37 +1,39 @@
-﻿public class Kata
-{
-    public static bool IsValidWalk(string[] walk)
-    {
-        if (walk.Length == 10)
-        {
-            int startX = 5;
-            int startY = 5;
-            int currentX = startX;
-            int currentY = startY;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+//[2, 4, 0, 100, 4, 11, 2602, 36]
+//Should return: 11 (the only odd number)
 
-            foreach (var step in walk)
-            {
-                switch (step)
-                {
-                    case "n":
-                        currentY += 1;
-                        break;
-                    case "s":
-                        currentY -= 1;
-                        break;
-                    case "w":
-                        currentX += 1;
-                        break;
-                    case "e":
-                        currentX -= 1;
-                        break;
-                }
-            }
-            return (currentX == startX && currentY == startY) ? true : false;
-        }
-        else
+//[160, 3, 1719, 19, 11, 13, -21]
+//Should return: 160 (the only even number)
+public class Kata
+{
+    public static int Find(int[] integers)
+    {
+        //int even = 0;
+        //int odd = 0;
+        //int lastNumber;
+        //foreach (var integer in integers)
+        //{
+        //    lastNumber = integer;
+        //    check = integer % 2 == 0;
+        //    switch (integer % 2)
+        //    {
+        //        case 0:
+        //            even++;
+        //            break;
+        //        default:
+        //            odd++;
+        //            break;
+        //    }
+        //}
+        List<int> evens = new List<int>();
+        List<int> odds = new List<int>();
+        foreach (var integer in integers)
         {
-            return false;
+            (integer % 2 == 0) ? evens.Add(integer) : odds.Add(integer);
+            if (evens.Count > odds.Count && odds.Count == 1) return odds.First();
+            if (odds.Count > evens.Count && evens.Count == 1) return evens.First();
         }
     }
 }
