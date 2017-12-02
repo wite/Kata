2cb82088b71b81b434e7a2a5456e154a714e6fbe
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 22:47:43 +0100

    7 kyu https://www.codewars.com/kata/5390bac347d09b7da40006f6

---
+++ JadenCasingStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        bool up = true;
+        for (int i = 0; i < phrase.Length; i++)
+        {
+            if (up)
+            {
+                phrase = phrase.Insert(i, phrase[i].ToString().ToUpper());
+                phrase = phrase.Remove(i + 1, 1);
+                up = false;
+            }
+            if (phrase[i] == ' ')
+            {
+                up = true;
+            }
+        }
+        return phrase;
+    }
+}
