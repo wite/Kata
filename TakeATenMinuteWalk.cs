29a74a484a6d7b9ed4c82f7a945df5cf3c96fbe8
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 11:52:48 +0100

    https://www.codewars.com/kata/54da539698b8a2ad76000228

---
+++ TakeATenMinuteWalk.cs
@@ -0,0 +1,37 @@
+﻿public class Kata
+{
+    public static bool IsValidWalk(string[] walk)
+    {
+        if (walk.Length == 10)
+        {
+            int startX = 5;
+            int startY = 5;
+            int currentX = startX;
+            int currentY = startY;
+
+            foreach (var step in walk)
+            {
+                switch (step)
+                {
+                    case "n":
+                        currentY += 1;
+                        break;
+                    case "s":
+                        currentY -= 1;
+                        break;
+                    case "w":
+                        currentX += 1;
+                        break;
+                    case "e":
+                        currentX -= 1;
+                        break;
+                }
+            }
+            return (currentX == startX && currentY == startY) ? true : false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
