2026cbf65fa4eb7c9b580d8cd2278291d44857de
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 23:43:33 +0100

    6 kyu https://www.codewars.com/kata/5287e858c6b5a9678200083c

---
+++ DoesMyNumberLookBigInThis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Kata
+{
+    public static bool Narcissistic(int value)
+    {
+        string valueAsString = value.ToString();
+        int power = valueAsString.Length;
+        double valueToCompare = 0;
+
+        foreach (var number in valueAsString)
+        {
+            int nm = (int)Char.GetNumericValue(number);
+            valueToCompare = valueToCompare + Math.Pow(nm, power);
+        }
+
+        return value == valueToCompare;
+    }
+}
