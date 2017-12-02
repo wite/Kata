f3b2c680731ab40a7bdf21f27d6fc10babc9ec6e
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-02 13:04:32 +0100

    6 kyu https://www.codewars.com/kata/54da539698b8a2ad76000228

--- TakeATenMinuteWalk.cs
+++ TakeATenMinuteWalk.cs
@@ -1,39 +1,37 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System;
-//[2, 4, 0, 100, 4, 11, 2602, 36]
-//Should return: 11 (the only odd number)
-
-//[160, 3, 1719, 19, 11, 13, -21]
-//Should return: 160 (the only even number)
-public class Kata
+﻿public class Kata
 {
-    public static int Find(int[] integers)
+    public static bool IsValidWalk(string[] walk)
     {
-        //int even = 0;
-        //int odd = 0;
-        //int lastNumber;
-        //foreach (var integer in integers)
-        //{
-        //    lastNumber = integer;
-        //    check = integer % 2 == 0;
-        //    switch (integer % 2)
-        //    {
-        //        case 0:
-        //            even++;
-        //            break;
-        //        default:
-        //            odd++;
-        //            break;
-        //    }
-        //}
-        List<int> evens = new List<int>();
-        List<int> odds = new List<int>();
-        foreach (var integer in integers)
+        if (walk.Length == 10)
         {
-            (integer % 2 == 0) ? evens.Add(integer) : odds.Add(integer);
-            if (evens.Count > odds.Count && odds.Count == 1) return odds.First();
-            if (odds.Count > evens.Count && evens.Count == 1) return evens.First();
+            int startX = 5;
+            int startY = 5;
+            int currentX = startX;
+            int currentY = startY;
+
+            foreach (var step in walk)
+            {
+                switch (step)
+                {
+                    case "n":
+                        currentY += 1;
+                        break;
+                    case "s":
+                        currentY -= 1;
+                        break;
+                    case "w":
+                        currentX += 1;
+                        break;
+                    case "e":
+                        currentX -= 1;
+                        break;
+                }
+            }
+            return (currentX == startX && currentY == startY) ? true : false;
+        }
+        else
+        {
+            return false;
         }
     }
 }
