6da3a547e37e0f783be3da4d9bdcb620c66726f3
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-03 23:36:24 +0100

    6 kyu https://www.codewars.com/kata/5626b561280a42ecc50000d1

---
+++ TakeANumberAndSumItsDigitsRaised.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SumDigPower
+{
+
+    public static long[] SumDigPow(long a, long b)
+    {
+        List<long> numbersWithProperty = new List<long>();
+        for (long x = a; x <= b; x++)
+        {
+            string valueAsString = x.ToString();
+            long power = valueAsString.Length;
+            double valueSecond = 0;
+            for (int i = 0; i < power; i++)
+            {
+                valueSecond += Math.Pow(Char.GetNumericValue(valueAsString[i]), i + 1);
+            }
+            if (x == valueSecond) numbersWithProperty.Add(x);
+        }
+        return numbersWithProperty.ToArray();
+    }
+}
