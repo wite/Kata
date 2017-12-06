fbc55937f11b11728ff344a1b92b6ff06034ac0b
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-06 22:19:40 +0100

    6 kyu https://www.codewars.com/kata/587731fda577b3d1b0001196/

---
+++ CamelCaseMethod.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+namespace Kata
+{
+    public static class Problem
+    {
+        public static string CamelCase(this string str)
+        {
+            TextInfo text = CultureInfo.CurrentCulture.TextInfo;
+            return text.ToTitleCase(str.ToLower()).Replace(" ", "");
+        }
+    }
+}
