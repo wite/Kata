ebe68c87ae205a35c3c5500e24331c45e740679a
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-16 04:11:04 +0100

    5 kyu https://www.codewars.com/kata/530e15517bc88ac656000716

---
+++ Rot13.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Kata
+{
+    public static string Rot13(string message)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string rot13 = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+        List<char> encodedMessage = new List<char>();
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            int alphabetIndex = alphabet.IndexOf(message[i]);
+            if (alphabetIndex >= 0) encodedMessage.Add(rot13[alphabetIndex]);
+            else encodedMessage.Add(message[i]);
+        }
+
+        return new string(encodedMessage.ToArray());
+    }
+}
