eb3500b5c526195a63d5b2ab3db99b8d26081bb6
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-16 02:01:50 +0100

    5 kyu https://www.codewars.com/kata/52685f7382004e774f0001f7

---
+++ HumanReadableTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public static class TimeFormat
+{
+    public static string GetReadableTime(int seconds)
+    {
+        var time = TimeSpan.FromSeconds(seconds);
+        var totalHours = time.Days * 24 + time.Hours;
+
+        return String.Format("{0}:{1}:{2}", totalHours.ToString("00"), time.Minutes.ToString("00"), time.Seconds.ToString("00"));
+    }
+}
