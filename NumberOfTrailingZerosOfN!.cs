f7365b12a9cc5dc34465325a40818f8f66014795
Witold Kryszak <kryszak.witold@gmail.com>
2017-12-28 23:45:00 +0100

    5 kyu https://www.codewars.com/kata/52f787eb172a8b4ae1000a34/

---
+++ NumberOfTrailingZerosOfN!.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int TrailingZeros(int n)
+    {
+        int counter;
+        int staticMultiplier = 5;
+        int multiplier = 5;
+        int trailingZeros = counter = n / staticMultiplier;
+        while (counter > 1)
+        {
+            multiplier *= staticMultiplier;
+            counter = n / multiplier;
+            trailingZeros += counter;
+        }
+        return trailingZeros;
+    }
+}
